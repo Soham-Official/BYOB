67b28ae982babb09fd76efa8b4dc5c26eea91ba9
Debatosh Pal Majumder <36793454+debatoshpm@users.noreply.github.com>
2021-10-21 19:38:14 +0530

    nav and contact template added

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
 import Loader from "./Components/Loader";
 import WhatDoWeDo from "./Components/WhatDoWeDo";
+// import Nav from "./Components/Nav";
+import Contact from "./Components/Contact";
 
 const Router = () => {
   const [loading, setLoading] = useState(true);
@@ -21,6 +23,7 @@ const Router = () => {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={WhatDoWeDo} />
+            <Route exact path="/contact" component={Contact} />
           </Switch>
         </BrowserRouter>
       )}
