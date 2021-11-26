da5e7bbe7c1da1b6096f94b053cb1c26653cb203
Soham Chakraborty <soham.chakrabortyofficial@gmail.com>
2021-11-27 01:36:17 +0530

    homepage image changed in mobile view

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,27 +6,27 @@ import { Link } from "react-router-dom";
 import Error from "./Error";
 const Contact = () => {
   const [error, setError] = useState(false);
-  function useWindowSize() {
-    const [size, setSize] = useState([0, 0]);
-    useLayoutEffect(() => {
-      function updateSize() {
-        setSize([window.innerWidth, window.innerHeight]);
-      }
-      window.addEventListener("resize", updateSize);
-      updateSize();
-      return () => window.removeEventListener("resize", updateSize);
-    }, []);
-    return size;
-  }
-  const [width] = useWindowSize();
-  useEffect(() => {
-    console.log(width);
-    if (width < 1024) {
-      setError(true);
-    } else {
-      setError(false);
-    }
-  }, [width]);
+  // function useWindowSize() {
+  //   const [size, setSize] = useState([0, 0]);
+  //   useLayoutEffect(() => {
+  //     function updateSize() {
+  //       setSize([window.innerWidth, window.innerHeight]);
+  //     }
+  //     window.addEventListener("resize", updateSize);
+  //     updateSize();
+  //     return () => window.removeEventListener("resize", updateSize);
+  //   }, []);
+  //   return size;
+  // }
+  // const [width] = useWindowSize();
+  // useEffect(() => {
+  //   console.log(width);
+  //   if (width < 1024) {
+  //     setError(true);
+  //   } else {
+  //     setError(false);
+  //   }
+  // }, [width]);
   const [name, setName] = useState("");
   const [mail, setMail] = useState("");
   const [num, setNum] = useState("");
